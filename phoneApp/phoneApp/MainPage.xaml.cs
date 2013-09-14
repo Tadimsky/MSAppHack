7c1e5a53f087b7a02879053eed0b765670647163
Kevin Oh <jo63@duke.edu>
2013-09-14 13:56:07 -0400

    created and opened a basic pushnotification channel

--- phoneApp/phoneApp/MainPage.xaml.cs
+++ phoneApp/phoneApp/MainPage.xaml.cs
@@ -7,18 +7,62 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Microsoft.Phone.Notification;
+
 
 namespace phoneApp
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        HttpNotificationChannel pushChannel;
+
+        string channelName = "pushChannel"; //CHANGE LATER
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-
-            // Set the data context of the listbox control to the sample data
             DataContext = App.ViewModel;
+
+            pushChannel = HttpNotificationChannel.Find(channelName);
+            // Set the data context of the listbox control to the sample data
+            // If the channel was not found, then create a new connection to the push service.
+            if (pushChannel == null)
+            {
+                pushChannel = new HttpNotificationChannel(channelName);
+
+                // Register for all the events before attempting to open the channel.
+                pushChannel.ChannelUriUpdated += new EventHandler<NotificationChannelUriEventArgs>(PushChannel_ChannelUriUpdated);
+ 
+                pushChannel.Open();
+
+            }
+            else
+            {
+                // The channel was already open, so just register for all the events.
+                pushChannel.ChannelUriUpdated += new EventHandler<NotificationChannelUriEventArgs>(PushChannel_ChannelUriUpdated);
+
+                // Display the URI for testing purposes. Normally, the URI would be passed back to your web service at this point.
+                System.Diagnostics.Debug.WriteLine(pushChannel.ChannelUri.ToString());
+                MessageBox.Show(String.Format("Channel Uri is {0}",
+                    pushChannel.ChannelUri.ToString()));
+
+            }
+
+            
+        }
+
+        //push nofiticaion retceived
+        void PushChannel_ChannelUriUpdated(object sender, NotificationChannelUriEventArgs e)
+        {
+
+            Dispatcher.BeginInvoke(() =>
+            {
+                // Display the new URI for testing purposes. Normally, the URI would be passed back to your web service at this point.
+                System.Diagnostics.Debug.WriteLine(e.ChannelUri.ToString());
+                MessageBox.Show(String.Format("Channel Uri is {0}",
+                    e.ChannelUri.ToString()));
+
+            });
         }
 
         // Load data for the ViewModel Items
